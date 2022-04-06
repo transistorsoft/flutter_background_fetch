65aaad8a0f3f61b98bfd2c0fd3c362f4d9e30883
Chris Scott <chris@transistorsoft.com>
2022-04-06 10:33:27 -0400

    [Android] Android 12 compatibility.  Add new required permission: android.permission.SCHEDULE_EXACT_ALARM

--- example/android/app/src/main/java/com/transistorsoft/flutter/backgroundfetch/backgroundfetchexample/Application.java
+++ example/android/app/src/main/java/com/transistorsoft/flutter/backgroundfetch/backgroundfetchexample/Application.java
@@ -1,15 +1,6 @@
 package com.transistorsoft.flutter.backgroundfetch.backgroundfetchexample;
 
-import android.util.Log;
-
-import androidx.annotation.NonNull;
-
-import com.transistorsoft.flutter.backgroundfetch.HeadlessTask;
-
 import io.flutter.app.FlutterApplication;
-import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.plugin.common.MethodCall;
-import io.flutter.plugin.common.MethodChannel;
 
 public class Application  extends FlutterApplication {
     @Override
@@ -33,25 +24,6 @@ public class Application  extends FlutterApplication {
         */
 
         super.onCreate();
-
-        ///
-        /// TEST onInitialized callback for custom MethodChannel
-        ///
-        /*
-        HeadlessTask.onInitialized(new HeadlessTask.OnInitializedCallback() {
-            @Override
-            public void onInitialized(FlutterEngine engine) {
-                Log.d("TSBackgroundFetch", "********* engine started: " + engine);
-                new MethodChannel(engine.getDartExecutor().getBinaryMessenger(), "channel_foo").setMethodCallHandler(new MethodChannel.MethodCallHandler() {
-                    @Override
-                    public void onMethodCall(@NonNull MethodCall call, @NonNull MethodChannel.Result result) {
-                        Log.d("TSBackgroundFetch", "**************** Application method call handler: " + call.method);
-                        result.success(true);
-                    }
-                });
-            }
-        });
-        */
 
     }
 }
